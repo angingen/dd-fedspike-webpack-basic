c21dea2aed91a839f1dfb67f572326665b1e179f
Jingwen Wang <jwang9@deloitte.com.au>
2023-08-03 21:21:41 +1000

    0: counter vanilla exercise and result

---
+++ 0-counter-vanilla-result/index.js
@@ -0,0 +1,10 @@
+(function () {
+  var counter = 0;
+  var increment = function () {
+    counter++;
+    document.querySelector(".js-counter-result").innerHTML = counter;
+  };
+
+  const button = document.querySelector(".js-counter-button");
+  button.addEventListener("click", increment);
+})();
